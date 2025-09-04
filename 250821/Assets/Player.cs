aa3f7dd6d14f580aea8d454e55841f5748c68638
JungHyon_1 <90584642+junghyonil123@users.noreply.github.com>
2025-09-04 19:17:33 +0900

    Feat: monster follow player

---
+++ 250821/Assets/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float speedOK = 0;
+
+    public void Attack(Slime slime)
+    {
+        slime.hp -= 1;
+        if(slime.hp <= 95)
+        {
+            speedOK = 1;
+        }
+    }
+
+    private void Update()
+    {
+        float Curspeed = speed;
+        if (speedOK == 1)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Curspeed = speed * 2;
+            }
+                
+        }
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * Curspeed;
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * Curspeed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Curspeed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Curspeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            Attack(collision.gameObject.GetComponent<Slime>());
+        }
+    }
+}
--- 250821/Assets/Slime.cs
+++ 250821/Assets/Slime.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class Slime : MonoBehaviour
 {
     public int hp = 100;
+    public float speed;
+
+    public Player target; 
+
+    private void Update()
+    {
+        Vector3 targetPos = target.gameObject.transform.position;
+        Vector3 myPos = transform.position;
+
+        Vector3 dirVec = (targetPos - myPos).normalized;
+
+        transform.position += dirVec * speed;
+        //target ~~~
+        //플레이어를 따라감
+
+    }
 }
